2e16265ce5caf9f4e5db0450db9bc3ef89c5e785
Chukajoseph99 <83855667+Chukajoseph99@users.noreply.github.com>
2021-05-27 22:30:42 +0100

    commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById("nav-ul");
+
+hamburger.addEventListener("click", ()=>{
+ navUL.classList.toggle("show"); 
+});
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
